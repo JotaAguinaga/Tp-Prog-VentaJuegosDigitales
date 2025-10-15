c741b76570e82de6f6270c664dad357bf86fb3ac
IvoPalmero <126575526+IvoPalmero@users.noreply.github.com>
2025-10-15 06:38:46 -0300

    Primer comit-clases

---
+++ VentaDeVideojuegos/Clases/Envio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases;
+public class Envio
+{
+    public int IdEnvio { get; set; }
+    public int IdPedido { get; set; }
+    public Pedido Pedido { get; set; }
+    public string DireccionEntrega { get; set; }
+}
