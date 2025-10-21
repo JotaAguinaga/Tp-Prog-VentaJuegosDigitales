acc8fbd0d6b313751278c3b81385ed5c4b41fe9f
JotaAguinaga <joaquin.aguinaga.ices@gmail.com>
2025-10-21 19:34:26 -0300

    Crear Repositorios para cada clase

--- Libreria de clases/Data/AplicationDbContext.cs
+++ Libreria de clases/Data/AplicationDbContext.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Clases;
 using Microsoft.EntityFrameworkCore;
 
 namespace Libreria_de_clases.Data
 {
     public class AplicationDbContext : DbContext
     {
+        public DbSet<Cliente> Clientes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
---
+++ Libreria de clases/Repositories/ClienteRepositorio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria_de_clases.Repositories
+{
+    public class ClienteRepositorio
+    {
+        private readonly Data.AplicationDbContext _context;
+        public ClienteRepositorio(Data.AplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void Agregar(Clases.Cliente cliente)
+        {
+            _context.Clientes.Add(cliente);
+            _context.SaveChanges();
+        }
+        public List<Clases.Cliente> ObtenerTodos()
+        {
+            return _context.Clientes.ToList();
+        }
+
+    }
+}
